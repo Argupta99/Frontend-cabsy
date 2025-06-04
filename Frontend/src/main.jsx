0646416350a16500f3b57c26918f3fee0bea30dd
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-06-04 22:08:08 +0530

    netlify

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import UserContext from './context/UserContext.jsx';
 import CaptainContext from './context/CaptainContext.jsx'; 
 import SocketProvider from './context/SocketContext.jsx';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <CaptainContext>
